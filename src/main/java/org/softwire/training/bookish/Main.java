a5d2bb511c27f69627156532456964ec074a8361
Jed Johnson <jed.johnson@softwire.com>
2019-11-05 10:45:36 +0000

    Can read temporary book table
    Can read SQL table using both methods

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,19 +1,19 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
-        String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String database = "test";
+        String user = "root";
+        String password = "password";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -23,25 +23,39 @@ public class Main {
     private static void jdbcMethod(String connectionString) throws SQLException {
         System.out.println("JDBC method...");
 
+        Statement stmt = null;
+        String query = "select id, title from test.book";
+        Connection connection = DriverManager.getConnection(connectionString);
+        try {
+
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                System.out.println(id + "\t" + title +"\t" );
+            }
+        } catch (SQLException e ) {
+//            JDBCTutorialUtilities.printSQLException(e);
+        } finally {
+            if (stmt != null) { stmt.close(); }
+        }
         // TODO: print out the details of all the books (using JDBC)
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
-
-        Connection connection = DriverManager.getConnection(connectionString);
-
-
-
     }
 
     private static void jdbiMethod(String connectionString) {
         System.out.println("\nJDBI method...");
 
         // TODO: print out the details of all the books (using JDBI)
-        // See this page for details: http://jdbi.org
-        // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
-
-
-
+        List<Book> books = jdbi.withHandle(handle ->
+             handle.createQuery("select id,title,author from test.csv_list")
+                     .mapToBean(Book.class)
+                     .list());
+        for (Book book : books) {
+            System.out.println(book);
+        }
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,37 @@
 package org.softwire.training.bookish.models.database;
 
-public class Book {
+public class Book{
 
+    private int id;
+    private String title;
+    private String author;
+
+    @Override
+    public String toString(){
+        return String.format("%-5d | %-20s | %-20s", id, title,author);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
